a7f76fa573c1f23837187d8078bb135a109c1d2c
Alex <eddings.sharanna@gmail.com>
2022-10-04 19:26:59 -0400

    finished example

---
+++ javascript-functions-lab.js
@@ -0,0 +1,23 @@
+// Requirements
+// Define and code the functions below.
+
+// Define the functions using the approach as specified (either as a function expression or declaration).
+
+// Be sure to number each function with a comment above it.
+
+// After each function, call it at least once and console.log the results.
+
+// 1. Define a function, as a function declaration, /maxOfTwoNumbers/ that takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
+//(Completed Example)
+function maxOfTwoNumbers(x, y) {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+
+  // or more "elegantly" using the fantastic ternary expression!
+  // return  x >= y ? x : y;
+}
+
+console.log(maxOfTwoNumbers(3, 9));
