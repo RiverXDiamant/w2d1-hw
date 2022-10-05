e75ebbcb72c4c4609609f15d2ff7960130ddc899
Alex <eddings.sharanna@gmail.com>
2022-10-04 20:49:32 -0400

    question 1 finished

--- javascript-functions-lab.js
+++ javascript-functions-lab.js
@@ -1,4 +1,5 @@
 // Requirements
+
 // Define and code the functions below.
 
 // Define the functions using the approach as specified (either as a function expression or declaration).
@@ -10,7 +11,7 @@
 // 1. (Completed Example)
 //  a. Define a function, as a function declaration /maxOfTwoNumbers/
 //  b. That takes two numbers as arguments and returns the largest of them.
-//  c. If they are the same, return that number. Use the if-else construct or a ternary expression - (the Math.max method is not allowed.)
+//  c. If they are the same, return that number. Use the if-else construct or a ternary expression - (the Math.max method is not allowed).
 //
 function maxOfTwoNumbers(x, y) {
   if (x >= y) {
@@ -24,3 +25,36 @@ function maxOfTwoNumbers(x, y) {
 }
 
 console.log(maxOfTwoNumbers(3, 9));
+
+// 2.
+//  a. Define a function, as a function expression, maxOfThree
+//  b. That takes three numbers as arguments
+//  c. Returns the largest of them. Again, the (Math.max method is not allowed).
+//
+// First Attempt:
+// const maxOfThree = (a, b, c) => {
+//   if (a >= c) {
+//     return a;
+//   } else if (b >= c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
+// };
+//
+//
+// console.log(maxOfThree(4, 17, 27));
+
+// Second Attempt:
+// const maxOfThree = (a, b, c) => {
+//   if (a >= c) return a;
+//   if (b >= c) return b;
+//   else return c;
+// };
+
+// console.log(maxOfThree(4, 17, 27));
+
+//Using Ternary Operator
+const maxOfThree = (a, b, c) => (a >= c ? a : c, b >= c ? b : c, c ? c : c);
+
+console.log(maxOfThree(4, 17, 27));
